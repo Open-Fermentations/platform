5055e0dc3cacee42fe71f55093c26fed4cee90d0
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2026-04-09 12:29:56 +0200

    feat: create stript to rollback a migration
    closes #44

---
+++ scripts/rollback-migration.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+. ./scripts/env.sh
+
+docker run -v --rm \
+-v ./migrations:/migrations \
+--network ${NAME}_open_fermentations \
+migrate/migrate \
+-path=/migrations/ \
+-database ${DB_CONNECTION_STRING} \
+down 1
