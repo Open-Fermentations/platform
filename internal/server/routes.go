a08aa6982ddd333ecacece94cebdb439329dea36
Kevin Heritage <8116780+slugger7@users.noreply.github.com>
2026-07-15 17:57:11 +0200

    chore: remove hello world handler

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -19,7 +19,6 @@ func (s *Server) RegisterRoutes() http.Handler {
 	mux := http.NewServeMux()
 
 	// Register routes
-	mux.HandleFunc("/", s.HelloWorldHandler)
 	mux.HandleFunc("/login", s.LoginHandler)
 
 	mux.HandleFunc("/health", s.healthHandler)
@@ -47,19 +46,6 @@ func (s *Server) corsMiddleware(next http.Handler) http.Handler {
 		// Proceed with the next handler
 		next.ServeHTTP(w, r)
 	})
-}
-
-func (s *Server) HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	resp := map[string]string{"message": "Hello World"}
-	jsonResp, err := json.Marshal(resp)
-	if err != nil {
-		http.Error(w, "Failed to marshal response", http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	if _, err := w.Write(jsonResp); err != nil {
-		slog.Error("failed to write response", slog.String("error", err.Error()))
-	}
 }
 
 func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
